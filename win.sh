8803b6e3145521ce56fbae2b02f574e3f0c253e1
daboynb <106079917+daboynb@users.noreply.github.com>
2023-05-23 15:19:07 +0200

    Update win.sh

--- win.sh
+++ win.sh
@@ -9,7 +9,7 @@ sudo fdisk -l
 read -r -p "Enter the disk name without the partitions number (example /dev/sdx): " DEVICE
 
 PARTITION_START="1M"
-PARTITION_END="12GB"
+PARTITION_END="6GB"
 MOUNT_DIR="/mnt"
 
 sudo mkdir -p "/mnt/WIN"
@@ -32,7 +32,7 @@ sudo partprobe $DEVICE
 sudo mkfs.ntfs -f -L "Main Partition" "${DEVICE}1"
 
 # Create the FAT16 partition
-sudo parted -s $DEVICE mkpart primary fat16 13GB 13GB+1MB
+sudo parted -s $DEVICE mkpart primary fat16 6GB 7GB+1MB
 
 # Inform the kernel of the changes to the partition table
 sudo partprobe $DEVICE
@@ -50,6 +50,9 @@ wget -P ./resources https://raw.githubusercontent.com/pbatard/rufus/master/res/u
 
 # dd of uefi.img
 sudo dd if='./resources/uefi-ntfs.img' of="${DEVICE}2"
+
+# Inform the kernel of the changes to the partition table
+sudo partprobe $DEVICE
 
 # Print information about the created partitions
 sudo parted $DEVICE print
